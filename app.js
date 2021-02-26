2d19642c6e8a56e2e6eeadac2d32393d7b06eb56
Sayam Kumar <55996885+Sayam2801@users.noreply.github.com>
2021-02-27 05:28:50 +0530

    New Commit

--- app.js
+++ app.js
@@ -1,65 +1,35 @@
 //jshint esversion:6
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workListItems = [];
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
+
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine','ejs');
-app.get("/", function(req,res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  // if(currentDay === 0 || currentDay === 6)
-  // {
-  //   // res.sendFile(__dirname + "/weekend.html");
-  //   day="Weekend";
-  // }
-  // else {
-  //   // res.sendFile(__dirname + "/weekday.html");
-  //   day="Weekday";
-  // }
-   // switch(currentDay)
-   // {
-   //   case 0:
-   //          day="Sunday";
-   //          break;
-   //   case 1:
-   //          day="Monday";
-   //          break;
-   //   case 2:
-   //          day="Tuesday";
-   //          break;
-   //   case 3:
-   //          day="Wednesday";
-   //          break;
-   //   case 4:
-   //          day="Thursday";
-   //          break;
-   //   case 5:
-   //          day="Friday";
-   //          break;
-   //   case 6:
-   //          day="Saturday";
-   //          break;
-   //   default:
-   //          console.log("Error! Current day is " + currentDay);
-   // }
-   var options = {
-     weekday: "long",
-     day: "numeric",
-     month: "long"
-   };
-   var day = today.toLocaleDateString("en-US", options);
-   res.render("list", {
-     kindOfDay:day,
-     newListItems:items
-   });
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", 'ejs');
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  res.render("list", {listTitle: day, newListItems: items});
 });
-app.post("/", function(req,res) {
-  var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+app.post("/",function(req,res) {
+  if(req.body.list === "Work"){
+    workListItems.push(req.body.newItem);
+    res.redirect("/work");
+  }
+  else {
+    items.push(req.body.newItem);
+    res.redirect("/");
+  }
+});
+app.get("/work", function(req,res) {
+  res.render("list", {listTitle: "Work List", newListItems: workListItems});
+});
+app.get("/about", function(req,res) {
+  res.render("about");
 });
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
